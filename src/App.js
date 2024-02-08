2808eb0d1fed44c8fbefb3ba55c6f198c8a37a3d
AlekseyResultSCHool <pashkovaa@mail.ru>
2024-02-08 21:39:44 +0500

    Настроить базу данных

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-cog"></i>
+			<div>4567</div>	
+		</Div>
+	);
+};
