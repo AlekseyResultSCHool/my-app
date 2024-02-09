492e10d095f9c309afd2397991e9862a9227bd26
AlekseyResultSCHool <pashkovaa@mail.ru>
2024-02-09 21:20:01 +0500

    Создали BFF

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
 
+
 const Div = styled.div`
 	text-align: center;
 `;
 
+
+
+
+
 export const App = () => {
+
+
 	return (
 		<Div>
 			<i className="fa fa-cog"></i>
